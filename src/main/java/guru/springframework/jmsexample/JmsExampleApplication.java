5e345843a5419ad4e7da37c1077eed4988501540
Brian Edwards <brianledwards@gmail.com>
2020-03-14 10:21:17 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/jmsexample/JmsExampleApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jmsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsExampleApplication.class, args);
+	}
+
+}
